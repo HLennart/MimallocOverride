d36b67137f722c62e5af164392fd689863977d41
HLennart <lennart.heggemann@gmail.com>
2024-04-24 16:34:14 +0200

    mimalloc override code & binaries

---
+++ Source/MimallocOverride/MimallocOverride.Build.cs
@@ -0,0 +1,64 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+using System.IO;
+using UnrealBuildTool;
+
+public class MimallocOverride : ModuleRules
+{
+	public MimallocOverride(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+				
+		if (Target.Platform == UnrealTargetPlatform.Win64) {
+			// PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "lib", "Win64", "mimalloc-override.dll"));
+			//PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "lib", "Win64", "mimalloc-redirect.dll"));
+			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "lib", "Win64", "mimalloc-redirect.lib"));
+			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "lib", "Win64", "mimalloc.lib"));
+			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "lib", "Win64", "mimalloc-override.lib"));
+			//PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "lib", "Win64", "mimalloc-redirect.dll"));
+		
+			RuntimeDependencies.Add("$(TargetOutputDir)/mimalloc-override.dll", Path.Combine(ModuleDirectory, "lib", "Win64", "mimalloc-override.dll"));
+			RuntimeDependencies.Add("$(TargetOutputDir)/mimalloc-redirect.dll", Path.Combine(ModuleDirectory, "lib", "Win64", "mimalloc-redirect.dll"));
+			RuntimeDependencies.Add("$(TargetOutputDir)/mimalloc.dll", Path.Combine(ModuleDirectory, "lib", "Win64", "mimalloc.dll"));
+		}
+		
+		PublicIncludePaths.AddRange(
+			new string[] {
+				// ... add public include paths required here ...
+			}
+			);
+				
+		
+		PrivateIncludePaths.AddRange(
+			new string[] {
+				// ... add other private include paths required here ...
+			}
+			);
+			
+		
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				// ... add other public dependencies that you statically link with here ...
+			}
+			);
+			
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				// ... add private dependencies that you statically link with here ...	
+			}
+			);
+		
+		
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+				// ... add any modules that your module loads dynamically here ...
+			}
+			);
+	}
+}
---
+++ Source/MimallocOverride/Private/MimallocOverride.cpp
@@ -0,0 +1,22 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MimallocOverride.h"
+#include "mimalloc.h"
+
+#define LOCTEXT_NAMESPACE "FMimallocOverrideModule"
+
+void FMimallocOverrideModule::StartupModule()
+{
+	mi_version();
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+}
+
+void FMimallocOverrideModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FMimallocOverrideModule, MimallocOverride)
